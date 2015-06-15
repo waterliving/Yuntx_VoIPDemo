cf02d689137248bfcce4172e7c2c4e4f369a2f5f
王 庚 <13641194007@139.com>
2015-06-15 11:12:44 +0800

    去掉 splashActivity

--- Yuntx_VoIPDemo_V4.0.1r/src/com/speedtong/example/voip/ui/ECLauncherUI.java
+++ Yuntx_VoIPDemo_V4.0.1r/src/com/speedtong/example/voip/ui/ECLauncherUI.java
@@ -13,16 +13,11 @@
 package com.speedtong.example.voip.ui;
 
 import java.io.InvalidClassException;
-
-
-
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -40,7 +35,6 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
-
 import com.speedtong.example.voip.ECApplication;
 import com.speedtong.example.voip.R;
 import com.speedtong.example.voip.common.utils.ECPreferenceSettings;
@@ -69,24 +63,19 @@ import com.umeng.analytics.MobclickAgent;
  * @date 2014-12-4
  * @version 4.0
  */
-public class ECLauncherUI extends FragmentActivity implements
-		View.OnClickListener {
-
+public class ECLauncherUI extends FragmentActivity implements View.OnClickListener {
 	/**
 	 * 当前ECLauncherUI 实例
 	 */
 	public static ECLauncherUI mLauncherUI;
-
 	/**
 	 * 当前ECLauncherUI实例产生个数
 	 */
 	public static int mLauncherInstanceCount = 0;
-
 	/**
 	 * 当前主界面RootView
 	 */
 	public View mLauncherView;
-
 	/**
 	 * LauncherUI 主界面导航控制View ,包含二个View Tab按钮
 	 */
@@ -95,48 +84,38 @@ public class ECLauncherUI extends FragmentActivity implements
 	 * 二个TabView所对应的二个页面的适配器
 	 */
 	private CCPCustomViewPager mCustomViewPager;
-
 	/**
 	 * 沟通、联系人、群组适配器
 	 */
 	public LauncherViewPagerAdapter mLauncherViewPagerAdapter;
-
 	private OverflowHelper mOverflowHelper;
-
 	/**
 	 * 当前显示的TabView Fragment
 	 */
 	private int mCurrentItemPosition = -1;
-
 	/**
 	 * 会话界面(沟通)
 	 */
 	private static final int TAB_NET_VOIP = 0;
-
 	/**
 	 * 通讯录界面(联系人)
 	 */
 	private static final int TAB_NET_PHONE = 1;
-
 	/**
 	 * {@link CCPLauncherUITabView} 是否已经被初始化
 	 */
 	private boolean mTabViewInit = false;
-
 	/**
 	 * 缓存三个TabView
 	 */
 	private final HashMap<Integer, Fragment> mTabViewCache = new HashMap<Integer, Fragment>();
 	private OverflowItem[] mItems = new OverflowItem[4];
-
 	private KickOffReceiver kickoffReceiver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
 		if (mLauncherUI != null) {
-			LogUtil.e(LogUtil.getLogUtilsTag(ECLauncherUI.class),
-					"finish last LauncherUI");
+			LogUtil.e(LogUtil.getLogUtilsTag(ECLauncherUI.class), "finish last LauncherUI");
 			mLauncherUI.finish();
 		}
 		mLauncherUI = this;
@@ -149,32 +128,25 @@ public class ECLauncherUI extends FragmentActivity implements
 		MobclickAgent.setDebugMode(true);
 		// 设置页面默认为竖屏
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		
 		registerReceiver(new String[] { getPackageName() + "kickedoff" });
 	}
-	
-	private class  KickOffReceiver extends BroadcastReceiver{
 
+	private class KickOffReceiver extends BroadcastReceiver {
 		@Override
 		public void onReceive(Context context, Intent intent) {
-
-			if (intent != null
-					&& intent.getAction().equalsIgnoreCase(
-							getPackageName() + "kickedoff")) {
-
+			if (intent != null && intent.getAction().equalsIgnoreCase(getPackageName() + "kickedoff")) {
 				finish();
 			}
-
 		}
-		
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		unregisterReceiver(kickoffReceiver);
 	}
+
 	protected final void registerReceiver(String[] actionArray) {
 		if (actionArray == null) {
 			return;
@@ -188,7 +160,6 @@ public class ECLauncherUI extends FragmentActivity implements
 		}
 		registerReceiver(kickoffReceiver, intentfilter);
 	}
-
 	private boolean mInit = false;
 
 	private void initWelcome() {
@@ -196,7 +167,6 @@ public class ECLauncherUI extends FragmentActivity implements
 			mInit = true;
 			setContentView(R.layout.splash_activity);
 			new Handler().postDelayed(new Runnable() {
-
 				@Override
 				public void run() {
 					mInit = false;
@@ -212,23 +182,18 @@ public class ECLauncherUI extends FragmentActivity implements
 	private void initLauncherUIView() {
 		mLauncherView = getLayoutInflater().inflate(R.layout.main_tab, null);
 		setContentView(mLauncherView);
-
 		mTabViewInit = true;
 		initOverflowItems();
 		mCustomViewPager = (CCPCustomViewPager) findViewById(R.id.pager);
 		mCustomViewPager.setOffscreenPageLimit(3);
-
 		if (mLauncherUITabView != null) {
 			mLauncherUITabView.setOnUITabViewClickListener(null);
 			mLauncherUITabView.setVisibility(View.VISIBLE);
 		}
 		mLauncherUITabView = (CCPLauncherUITabView) findViewById(R.id.laucher_tab_top);
 		mCustomViewPager.setSlideEnabled(true);
-		mLauncherViewPagerAdapter = new LauncherViewPagerAdapter(this,
-				mCustomViewPager);
-		mLauncherUITabView
-				.setOnUITabViewClickListener(mLauncherViewPagerAdapter);
-
+		mLauncherViewPagerAdapter = new LauncherViewPagerAdapter(this, mCustomViewPager);
+		mLauncherUITabView.setOnUITabViewClickListener(mLauncherViewPagerAdapter);
 		findViewById(R.id.btn_plus).setOnClickListener(this);
 		ctrlViewTab(0);
 	}
@@ -240,31 +205,24 @@ public class ECLauncherUI extends FragmentActivity implements
 	 * @return
 	 */
 	public final BaseFragment getTabView(int tabIndex) {
-		LogUtil.d(LogUtil.getLogUtilsTag(ECLauncherUI.class), "get tab index "
-				+ tabIndex);
+		LogUtil.d(LogUtil.getLogUtilsTag(ECLauncherUI.class), "get tab index " + tabIndex);
 		if (tabIndex < 0) {
 			return null;
 		}
-
 		if (mTabViewCache.containsKey(Integer.valueOf(tabIndex))) {
 			return (BaseFragment) mTabViewCache.get(Integer.valueOf(tabIndex));
 		}
-
 		BaseFragment mFragment = null;
 		switch (tabIndex) {
 		case TAB_NET_VOIP:
-			mFragment = (TabFragment) Fragment.instantiate(this,
-					NetVoipFragment.class.getName(), null);
+			mFragment = (TabFragment) Fragment.instantiate(this, NetVoipFragment.class.getName(), null);
 			break;
 		case TAB_NET_PHONE:
-			mFragment = (TabFragment) Fragment.instantiate(this,
-					NetPhoneFragment.class.getName(), null);
+			mFragment = (TabFragment) Fragment.instantiate(this, NetPhoneFragment.class.getName(), null);
 			break;
-
 		default:
 			break;
 		}
-
 		if (mFragment != null) {
 			mFragment.setActionBarActivity(this);
 		}
@@ -279,30 +237,20 @@ public class ECLauncherUI extends FragmentActivity implements
 	 *            子Fragment对应的index
 	 */
 	public void ctrlViewTab(int index) {
-
-		LogUtil.d(LogUtil.getLogUtilsTag(ECLauncherUI.class), "change tab to "
-				+ index + ", cur tab " + mCurrentItemPosition
-				+ ", has init tab " + mTabViewInit + ", tab cache size "
-				+ mTabViewCache.size());
-		if ((!mTabViewInit || index < 0)
-				|| (mLauncherViewPagerAdapter != null && index > mLauncherViewPagerAdapter
-						.getCount() - 1)) {
+		LogUtil.d(LogUtil.getLogUtilsTag(ECLauncherUI.class), "change tab to " + index + ", cur tab " + mCurrentItemPosition + ", has init tab " + mTabViewInit + ", tab cache size " + mTabViewCache.size());
+		if ((!mTabViewInit || index < 0) || (mLauncherViewPagerAdapter != null && index > mLauncherViewPagerAdapter.getCount() - 1)) {
 			return;
 		}
-
 		if (mCurrentItemPosition == index) {
 			return;
 		}
 		mCurrentItemPosition = index;
-
 		if (mLauncherUITabView != null) {
 			mLauncherUITabView.doChangeTabViewDisplay(mCurrentItemPosition);
 		}
-
 		if (mCustomViewPager != null) {
 			mCustomViewPager.setCurrentItem(mCurrentItemPosition, false);
 		}
-
 	}
 
 	void initOverflowItems() {
@@ -312,8 +260,7 @@ public class ECLauncherUI extends FragmentActivity implements
 		String name = null;
 		try {
 			ClientUser clientUser = CCPAppManager.getClientUser();
-			name = ContactSqlManager.getContact(clientUser.getUserId())
-					.getNickname();
+			name = ContactSqlManager.getContact(clientUser.getUserId()).getNickname();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -333,30 +280,22 @@ public class ECLauncherUI extends FragmentActivity implements
 	@Override
 	public boolean dispatchKeyEvent(KeyEvent event) {
 		LogUtil.d(LogUtil.getLogUtilsTag(ECLauncherUI.class), " onKeyDown");
-
-		if ((event.getKeyCode() == KeyEvent.KEYCODE_BACK)
-				&& event.getAction() == KeyEvent.ACTION_UP) {
+		if ((event.getKeyCode() == KeyEvent.KEYCODE_BACK) && event.getAction() == KeyEvent.ACTION_UP) {
 			// dismiss PlusSubMenuHelper
 			if (mOverflowHelper != null && mOverflowHelper.isOverflowShowing()) {
 				mOverflowHelper.dismiss();
 				return true;
 			}
 		}
-
 		// 这里可以进行设置全局性的menu菜单的判断
-		if ((event.getKeyCode() == KeyEvent.KEYCODE_BACK)
-				&& event.getAction() == KeyEvent.ACTION_DOWN) {
+		if ((event.getKeyCode() == KeyEvent.KEYCODE_BACK) && event.getAction() == KeyEvent.ACTION_DOWN) {
 			doTaskToBackEvent();
 		}
-
 		try {
-
 			return super.dispatchKeyEvent(event);
 		} catch (Exception e) {
-			LogUtil.e(LogUtil.getLogUtilsTag(ECLauncherUI.class),
-					"dispatch key event catch exception " + e.getMessage());
+			LogUtil.e(LogUtil.getLogUtilsTag(ECLauncherUI.class), "dispatch key event catch exception " + e.getMessage());
 		}
-
 		return false;
 	}
 
@@ -366,7 +305,6 @@ public class ECLauncherUI extends FragmentActivity implements
 		super.onResume();
 		// 统计时长
 		MobclickAgent.onResume(this);
-
 		if (mLauncherUITabView == null) {
 			String account = getAutoRegistAccount();
 			if (TextUtils.isEmpty(account)) {
@@ -381,7 +319,6 @@ public class ECLauncherUI extends FragmentActivity implements
 			user.setUserToken(split[3]);
 			user.setUserName(split[4]);
 			CCPAppManager.setClientUser(user);
-
 			SDKCoreHelper.init(this);
 			// 初始化主界面Tab资源
 			if (!mInit) {
@@ -389,10 +326,8 @@ public class ECLauncherUI extends FragmentActivity implements
 			}
 		}
 		if (ECApplication.isNetWorkConnect(this)) {
-
 			ECApplication.getInstance().getDemoAccountInfos();
 		}
-
 	}
 
 	/**
@@ -401,11 +336,9 @@ public class ECLauncherUI extends FragmentActivity implements
 	 * @return
 	 */
 	private String getAutoRegistAccount() {
-		SharedPreferences sharedPreferences = ECPreferences
-				.getSharedPreferences();
+		SharedPreferences sharedPreferences = ECPreferences.getSharedPreferences();
 		ECPreferenceSettings registAuto = ECPreferenceSettings.SETTINGS_REGIST_AUTO;
-		String registAccount = sharedPreferences.getString(registAuto.getId(),
-				(String) registAuto.getDefaultValue());
+		String registAccount = sharedPreferences.getString(registAuto.getId(), (String) registAuto.getDefaultValue());
 		return registAccount;
 	}
 
@@ -413,15 +346,12 @@ public class ECLauncherUI extends FragmentActivity implements
 		if (mOverflowHelper == null) {
 			return;
 		}
-
 		if (mOverflowHelper.isOverflowShowing()) {
 			mOverflowHelper.dismiss();
 			return;
 		}
-
 		mOverflowHelper.setOverflowItems(mItems);
-		mOverflowHelper
-				.setOnOverflowItemClickListener(mOverflowItemCliclListener);
+		mOverflowHelper.setOnOverflowItemClickListener(mOverflowItemCliclListener);
 		mOverflowHelper.showAsDropDown(findViewById(R.id.btn_plus));
 	}
 
@@ -438,7 +368,6 @@ public class ECLauncherUI extends FragmentActivity implements
 	 */
 	public void doTaskToBackEvent() {
 		moveTaskToBack(true);
-
 	}
 
 	/**
@@ -448,17 +377,13 @@ public class ECLauncherUI extends FragmentActivity implements
 	 * @date 2014-12-4
 	 * @version 4.0
 	 */
-	private class LauncherViewPagerAdapter extends FragmentStatePagerAdapter
-			implements ViewPager.OnPageChangeListener,
-			CCPLauncherUITabView.OnUITabViewClickListener {
+	private class LauncherViewPagerAdapter extends FragmentStatePagerAdapter implements ViewPager.OnPageChangeListener, CCPLauncherUITabView.OnUITabViewClickListener {
 		/**
 		 * 
 		 */
 		private int mClickTabCounts;
-
 		private NetPhoneFragment netPhoneFragment;
 		private NetVoipFragment netVoipFragment;
-
 		private final ViewPager mViewPager;
 		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
 
@@ -483,7 +408,6 @@ public class ECLauncherUI extends FragmentActivity implements
 
 		public void addTab(String tabSpec, Class<?> clss, Bundle args) {
 			String tag = tabSpec;
-
 			TabInfo info = new TabInfo(tag, clss, args);
 			mTabs.add(info);
 			notifyDataSetChanged();
@@ -501,40 +425,30 @@ public class ECLauncherUI extends FragmentActivity implements
 
 		@Override
 		public void onPageScrollStateChanged(int state) {
-			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class),
-					"onPageScrollStateChanged state = " + state);
-
-			if (state != ViewPager.SCROLL_STATE_IDLE
-					|| netPhoneFragment == null) {
+			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class), "onPageScrollStateChanged state = " + state);
+			if (state != ViewPager.SCROLL_STATE_IDLE || netPhoneFragment == null) {
 				return;
 			}
-
 			netPhoneFragment = null;
 		}
 
 		@Override
-		public void onPageScrolled(int position, float positionOffset,
-				int positionOffsetPixels) {
-			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class),
-					"onPageScrolled " + position + " " + positionOffset + " "
-							+ positionOffsetPixels);
+		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class), "onPageScrolled " + position + " " + positionOffset + " " + positionOffsetPixels);
 			if (mLauncherUITabView != null) {
-				mLauncherUITabView.doTranslateImageMatrix(position,
-						positionOffset);
+				mLauncherUITabView.doTranslateImageMatrix(position, positionOffset);
 			}
 			if (positionOffset != 0.0F) {
 				if (netPhoneFragment == null) {
 					netPhoneFragment = (NetPhoneFragment) getTabView(CCPLauncherUITabView.TAB_VIEW_SECOND);
 				}
-
 				return;
 			}
 		}
 
 		@Override
 		public void onPageSelected(int position) {
-			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class),
-					"onPageSelected");
+			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class), "onPageSelected");
 			if (mLauncherUITabView != null) {
 				mLauncherUITabView.doChangeTabViewDisplay(position);
 				mCurrentItemPosition = position;
@@ -544,21 +458,16 @@ public class ECLauncherUI extends FragmentActivity implements
 		@Override
 		public void onTabClick(int tabIndex) {
 			if (tabIndex == mCurrentItemPosition) {
-				LogUtil.d(
-						LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class),
-						"on click same index " + tabIndex);
+				LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class), "on click same index " + tabIndex);
 				// Perform a rolling
 				TabFragment item = (TabFragment) getItem(tabIndex);
 				item.onTabFragmentClick();
 				return;
 			}
-
 			mClickTabCounts += mClickTabCounts;
-			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class),
-					"onUITabView Click count " + mClickTabCounts);
+			LogUtil.d(LogUtil.getLogUtilsTag(LauncherViewPagerAdapter.class), "onUITabView Click count " + mClickTabCounts);
 			mViewPager.setCurrentItem(tabIndex);
 		}
-
 	}
 
 	@Override
@@ -571,67 +480,41 @@ public class ECLauncherUI extends FragmentActivity implements
 	@Override
 	protected void onNewIntent(Intent intent) {
 		super.onNewIntent(intent);
-
 		Intent actionIntent = intent;
 		String userName = actionIntent.getStringExtra("Main_FromUserName");
-		ECContacts contacts = ContactSqlManager
-				.getContactLikeUsername(userName);
-
+		ECContacts contacts = ContactSqlManager.getContactLikeUsername(userName);
 	}
-
 	private final AdapterView.OnItemClickListener mOverflowItemCliclListener = new AdapterView.OnItemClickListener() {
-
 		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id) {
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 			controlPlusSubMenu();
 			if (position == 0) {
-				
-			
-
 			} else if (position == 1) {
-
 				String[] splitTestNumbers;
 				if (!TextUtils.isEmpty(CCPConfig.test_number)) {
 					splitTestNumbers = CCPConfig.test_number.split(",");
-
 					ArrayList<String> arrays = new ArrayList<String>();
 					for (String str : splitTestNumbers) {
-
 						arrays.add(str);
 					}
-
 					if (arrays != null && arrays.size() >= 3) {
-
 						ToastUtil.showMessage("当前最多绑定3个测试号码");
 						return;
 					}
-
 				}
-				startActivity(new Intent(ECLauncherUI.this,
-						ECBindTestPhoneUI.class));
-
+				startActivity(new Intent(ECLauncherUI.this, ECBindTestPhoneUI.class));
 			} else if (position == 2) {
-
 				try {
-					ECPreferences
-							.savePreference(
-									ECPreferenceSettings.SETTINGS_REGIST_AUTO,
-									"", true);
+					ECPreferences.savePreference(ECPreferenceSettings.SETTINGS_REGIST_AUTO, "", true);
 					SDKCoreHelper.logout();
-
-
-					 ECDevice.unInitial();
-
-					 finish();
-					 startActivity(new Intent(ECLauncherUI.this,
-					 ECLoginActivity.class));
+					ECDevice.unInitial();
+					finish();
+					startActivity(new Intent(ECLauncherUI.this, ECLoginActivity.class));
 				} catch (InvalidClassException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			} else if (position == 3) {
-
 				try {
 					// ECPreferences.savePreference(
 					// ECPreferenceSettings.SETTINGS_FULLY_EXIT, true,
@@ -639,27 +522,21 @@ public class ECLauncherUI extends FragmentActivity implements
 					// ECPreferences.savePreference(ECPreferenceSettings.SETTINGS_FULLY_EXIT,
 					// false, true);
 					SDKCoreHelper.logout();
-
 					ECDevice.unInitial();
 					finish();
 					android.os.Process.killProcess(android.os.Process.myPid());
-
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
 			}
 		}
-
 	};
 
 	public void onNetWorkNotify(Connect connectState) {
 		BaseFragment tabView = getTabView(TAB_NET_VOIP);
-		if(tabView instanceof NetVoipFragment && tabView.isAdded()) {
+		if (tabView instanceof NetVoipFragment && tabView.isAdded()) {
 			((NetVoipFragment) tabView).updateConnectState();
 		}
-		
 	}
-
 }
